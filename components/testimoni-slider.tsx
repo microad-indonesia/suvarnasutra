d30481a5b94ecaa375a697798d084fb020e50159
Herka Fitra <herka@microad.co.id>
2023-11-14 12:17:43 +0700

    new adjustment

--- components/testimoni-slider.tsx
+++ components/testimoni-slider.tsx
@@ -81,7 +81,7 @@ export default function SliderComponents() {
   const settings = {
     dots: true,
     infinite: false,
-    speed: 2000,
+    speed: 1500,
     slidesToShow: 1,
     slidesToScroll: 1,
     row: 3,
@@ -94,99 +94,6 @@ export default function SliderComponents() {
     <section className="relative">
       <div className="block max-w-sm md:max-w-3xl lg:max-w-5xl xl:max-w-7xl my-8 md:my-10">
         <Slider {...settings}>
-          {/* Bellflower */}
-          <div className="bg-transparant  rounded-xl p-8 w-[320px] h-fit">
-            <div className="flex flex-wrap justify-evenly">
-              <div className="flex-initial">
-                <figure className="w-full h-fit">
-                  <Image
-                    src={HomeImageBellflower}
-                    alt="denah-rumah-bellflower"
-                    width={457}
-                    height={524}
-                  />
-                </figure>
-              </div>
-
-              <div className="flex-initial space-y-6 max-w-[186px] mt-3 md:mt-0">
-                <div className="space-y-4">
-                  <figure className="w-fit h-fit">
-                    <Image
-                      src={LogoBellflower}
-                      alt="logo-bellflower"
-                      width={185}
-                      height={64}
-                    />
-                  </figure>
-                  <p className="paragraph text-sakura-brown">
-                    LT : 96 m² (8x12)
-                  </p>
-                  <p className="paragraph text-sakura-brown">LB : 122 m²</p>
-                </div>
-
-                {/* list facilities */}
-                <div className="flex-1 space-y-4">
-                  <div className="flex items-center gap-x-2">
-                    {React.createElement(Unit1, {
-                      width: "46px",
-                      height: "37px",
-                    })}
-                    <p>4+1 KT</p>
-                  </div>
-                  <div className="flex items-center gap-x-2">
-                    {React.createElement(Unit2, {
-                      width: "46px",
-                      height: "37px",
-                    })}
-                    <p>3+1 KM</p>
-                  </div>
-                  <div className="flex items-center gap-x-2">
-                    {React.createElement(Unit3, {
-                      width: "46px",
-                      height: "37px",
-                    })}
-                    <p>1 WALK IN CLOSET</p>
-                  </div>
-                  <div className="flex items-center gap-x-2">
-                    {React.createElement(Unit4, {
-                      width: "50px",
-                      height: "20px",
-                    })}
-                    <p>2 MOBIL</p>
-                  </div>
-                  {/* <div className="flex items-center gap-x-2">
-                    {React.createElement(Unit5, {
-                      width: "48px",
-                      height: "35px",
-                    })}
-                    <p>1 MOTOR</p>
-                  </div> */}
-                </div>
-              </div>
-
-              <div className="flex items-center justify-center mt-3 md:mt-0">
-                <div className="flex-1 text-center">
-                  <figure className="w-full h-fit">
-                    <Image
-                      src={Denah1Bellflower}
-                      alt="denah-rumah-bellflower-1"
-                    />
-                  </figure>
-                  <p>Lantai Bawah</p>
-                </div>
-                <div className="flex-1 text-center">
-                  <figure className="w-full h-fit">
-                    <Image
-                      src={Denah2Bellflower}
-                      alt="denah-rumah-bellflower-2"
-                    />
-                  </figure>
-                  <p>Lantai Atas</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
           {/* Bloom done */}
           <div className="bg-transparant  rounded-xl p-8 w-[320px] h-fit">
             <div className="flex flex-wrap justify-evenly">
@@ -262,7 +169,7 @@ export default function SliderComponents() {
                   <figure className="w-full h-fit">
                     <Image src={Denah1} alt="denah-rumah-bloom-1" />
                   </figure>
-                  <p>Lantai Bawah</p>
+                  <p>Lantai Dasar</p>
                 </div>
                 <div className="flex-1 text-center">
                   <figure className="w-full h-fit">
@@ -349,11 +256,104 @@ export default function SliderComponents() {
                   <figure className="w-full h-fit">
                     <Image src={Denah1Burnet} alt="denah-rumah-burnet-1" />
                   </figure>
-                  <p>Lantai Bawah</p>
+                  <p>Lantai Dasar</p>
                 </div>
                 <div className="flex-1 text-center">
                   <figure className="w-full h-fit">
                     <Image src={Denah2Burnet} alt="denah-rumah-burnet-2" />
+                  </figure>
+                  <p>Lantai Atas</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Bellflower */}
+          <div className="bg-transparant  rounded-xl p-8 w-[320px] h-fit">
+            <div className="flex flex-wrap justify-evenly">
+              <div className="flex-initial">
+                <figure className="w-full h-fit">
+                  <Image
+                    src={HomeImageBellflower}
+                    alt="denah-rumah-bellflower"
+                    width={457}
+                    height={524}
+                  />
+                </figure>
+              </div>
+
+              <div className="flex-initial space-y-6 max-w-[186px] mt-3 md:mt-0">
+                <div className="space-y-4">
+                  <figure className="w-fit h-fit">
+                    <Image
+                      src={LogoBellflower}
+                      alt="logo-bellflower"
+                      width={185}
+                      height={64}
+                    />
+                  </figure>
+                  <p className="paragraph text-sakura-brown">
+                    LT : 96 m² (8x12)
+                  </p>
+                  <p className="paragraph text-sakura-brown">LB : 122 m²</p>
+                </div>
+
+                {/* list facilities */}
+                <div className="flex-1 space-y-4">
+                  <div className="flex items-center gap-x-2">
+                    {React.createElement(Unit1, {
+                      width: "46px",
+                      height: "37px",
+                    })}
+                    <p>4+1 KT</p>
+                  </div>
+                  <div className="flex items-center gap-x-2">
+                    {React.createElement(Unit2, {
+                      width: "46px",
+                      height: "37px",
+                    })}
+                    <p>3+1 KM</p>
+                  </div>
+                  <div className="flex items-center gap-x-2">
+                    {React.createElement(Unit3, {
+                      width: "46px",
+                      height: "37px",
+                    })}
+                    <p>1 WALK IN CLOSET</p>
+                  </div>
+                  <div className="flex items-center gap-x-2">
+                    {React.createElement(Unit4, {
+                      width: "50px",
+                      height: "20px",
+                    })}
+                    <p>2 MOBIL</p>
+                  </div>
+                  {/* <div className="flex items-center gap-x-2">
+                    {React.createElement(Unit5, {
+                      width: "48px",
+                      height: "35px",
+                    })}
+                    <p>1 MOTOR</p>
+                  </div> */}
+                </div>
+              </div>
+
+              <div className="flex items-center justify-center mt-3 md:mt-0">
+                <div className="flex-1 text-center">
+                  <figure className="w-full h-fit">
+                    <Image
+                      src={Denah1Bellflower}
+                      alt="denah-rumah-bellflower-1"
+                    />
+                  </figure>
+                  <p>Lantai Dasar</p>
+                </div>
+                <div className="flex-1 text-center">
+                  <figure className="w-full h-fit">
+                    <Image
+                      src={Denah2Bellflower}
+                      alt="denah-rumah-bellflower-2"
+                    />
                   </figure>
                   <p>Lantai Atas</p>
                 </div>
